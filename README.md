56247e3b7af26cc9036096d4cf25c212d2a80e22
PolarGoose <35307286+PolarGoose@users.noreply.github.com>
2025-02-19 20:43:56 +0100

    Update to the version 19.1.7

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # ClangFormat for Windows
-Repository to produce statically linked x64 binary of [clang-format](https://clang.llvm.org/docs/ClangFormat.html)<br>
-Download the binary from the [Releases page](https://github.com/PolarGoose/clang-format-for-Windows/releases)
+Repository to produce statically linked x64 binary of [clang-format](https://clang.llvm.org/docs/ClangFormat.html)<br>.
+
+# How to download
+[Releases page](https://github.com/PolarGoose/clang-format-for-Windows/releases)
 
 # How to build
-Run `build.ps1` script to build the binary.
+Run `build.ps1` script. You need to have Visual Studio installed
--- build.ps1
+++ build.ps1
@@ -26,28 +26,29 @@ Info "Find Visual Studio installation path"
 $vswhereCommand = Get-Command -Name "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 $installationPath = & $vswhereCommand -prerelease -latest -property installationPath
 
-Info "Open Visual Studio 2022 Developer PowerShell"
-& "$installationPath\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64
-
 Info "Remove '$buildDir' folder if it exists"
 Remove-Item $buildDir -Force -Recurse -ErrorAction SilentlyContinue
 New-Item $buildDir -Force -ItemType "directory" > $null
 
 Info "Download llvm source code"
-Invoke-WebRequest -Uri https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-18.1.6.zip -OutFile $buildDir/llvm.zip
+Invoke-WebRequest -Uri https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-19.1.7.zip -OutFile $buildDir/llvm.zip
 
 Info "Extract the source code"
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$buildDir/llvm.zip", "$buildDir")
 
+Info "Open Visual Studio 2022 Developer PowerShell"
+& "$installationPath\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64
+
 Info "Cmake generate cache"
 cmake `
-  -S $buildDir/llvm-project-llvmorg-18.1.6/llvm `
+  -S $buildDir/llvm-project-llvmorg-19.1.7/llvm `
   -B $buildDir/out `
   -G "Ninja" `
-  -DLLVM_TARGETS_TO_BUILD="AArch64" `
-  -DLLVM_ENABLE_PROJECTS="clang" `
-  -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded `
-  -DCMAKE_BUILD_TYPE=Release
+  -D LLVM_TARGETS_TO_BUILD="AArch64" `
+  -D LLVM_ENABLE_PROJECTS="clang" `
+  -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded `
+  -D CMAKE_ASM_MASM_FLAGS="/nologo" `
+  -D CMAKE_BUILD_TYPE=Release
 CheckReturnCodeOfPreviousCommand "cmake generate cache failed"
 
 Info "Cmake build"
